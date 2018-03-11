aabd4f1793980c338787f72e5179c4067fd3804f
Robin de Kort <Robindekort@hotmail.com>
2018-03-11 01:10:12 +0100

    adjustments to init

---
+++ src/main/java/kwetter/HelloApplication.java
@@ -0,0 +1,13 @@
+package kwetter;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author ievans
+ */
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
--- src/main/java/kwetter/HelloWorld.java
+++ src/main/java/kwetter/HelloWorld.java
@@ -1,11 +1,4 @@
-/**
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
-package fontys.kwetter;
+package kwetter;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
